a8e2969aec2f8c7e72fcf77eb8ec9deff4ef78b6
Vlad <vladvolkov03@mail.ru>
2022-07-23 17:10:34 +0300

    normal frame

--- server.go
+++ server.go
@@ -1,16 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginframe/controller"
+	"ginframe/service"
+
+	"github.com/gin-gonic/gin"
+)
 
 
+var (
+	videoService service.VideoService = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
+)
 func main(){
 
 	server := gin.Default()
 
-	server.GET("/test",func(ctx *gin.Context) {
-		ctx.JSON(200,gin.H{
-			"message":"OK!",
-		})
+	server.GET("/post",func(ctx *gin.Context) {
+		ctx.JSON(200,VideoController.FindAll())
+	})
+
+	server.POST("/post",func(ctx *gin.Context) {
+		ctx.JSON(200,VideoController.Save(ctx))
 	})
 	server.Run(":8080")
 }
